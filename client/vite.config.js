6f0997270504e7f26a5b6aabe322a00417471579
amarkumarswarg-design <amarkumarswarg@gmail.com>
2026-01-21 02:59:57 +0530

    Create vite.config.js

---
+++ client/vite.config.js
@@ -0,0 +1,33 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+      '/socket.io': {
+        target: 'http://localhost:5000',
+        ws: true,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['tailwindcss', '@heroicons/react'],
+          utils: ['axios', 'socket.io-client'],
+        }
+      }
+    }
+  }
+})
